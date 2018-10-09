ed411b196d497bc3660a27bc8855349110003c80
adamszabolcs <szabolcs.adam91@gmail.com>
2018-10-09 13:54:18 +0200

    created constructor, getters

--- src/main/java/com/codecool/items/Item.java
+++ src/main/java/com/codecool/items/Item.java
@@ -1,4 +1,22 @@
 package com.codecool.items;
 
 public class Item {
+
+    private int id;
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.id = id++;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
