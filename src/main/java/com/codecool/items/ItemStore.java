ceba15b549579ac141397101a054d77c58cc4a3c
adamszabolcs <szabolcs.adam91@gmail.com>
2018-10-09 13:54:54 +0200

    created add and remove method

--- src/main/java/com/codecool/items/ItemStore.java
+++ src/main/java/com/codecool/items/ItemStore.java
@@ -1,4 +1,17 @@
 package com.codecool.items;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemStore {
+
+    public static List<Item> itemList = new ArrayList<>();
+
+    private static void add(Item item) {
+        itemList.add(item);
+    }
+
+    private static void remove(Item item) {
+        itemList.remove(item);
+    }
 }
