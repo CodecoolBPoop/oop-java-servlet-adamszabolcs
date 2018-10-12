604df2c44c3dc09fb0bb98c441c6a4e75d5af3bd
adamszabolcs <szabolcs.adam91@gmail.com>
2018-10-12 17:38:10 +0200

    created remove button for items, added javascript and jquery

--- src/main/java/com/codecool/servlet/ShoppingCartServlet.java
+++ src/main/java/com/codecool/servlet/ShoppingCartServlet.java
@@ -11,11 +11,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-@WebServlet(name = "cart", urlPatterns = {"/cart"}, loadOnStartup = 2)
+@WebServlet(name = "cart", urlPatterns = {"/cart"})
 public class ShoppingCartServlet extends HttpServlet {
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
         List<Item> cartList = ItemStore.getItemList();
         PrintWriter out = response.getWriter();
         String title = "Cart page";
@@ -34,16 +34,23 @@ public class ShoppingCartServlet extends HttpServlet {
         cartPage.append("</tr>");
         for (Item item : cartList) {
             cartPage.append("<tr><td>" + item.getName() + "</td>");
-            cartPage.append("<td>" + item.getPrice() + "</td></tr>");
+            cartPage.append("<td>" + item.getPrice() + "</td>");
+            cartPage.append("<td><button class=\"remove\" id=\"" + item.getId() + "\">Remove</button></td></tr>");
         }
 
         out.println(
                 "<html>" +
-                "<head><title>" + title + "</title></head>" +
+                "<head><title>" + title + "</title>" +
+                        "<script type=\"text/javascript\" src=\"static/javascript/buttonHandler.js\" defer></script>\n" +
+                        "<script src=\"https://code.jquery.com/jquery-3.3.1.js\"" +
+                        "integrity=\"sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=\"" +
+                        "crossorigin=\"anonymous\"></script>" +
+                        "</head>" +
                 "<body>" +
                 "<h1 align=\"center\">" + title + "</h1>" +
                 "<div>" + cartPage.toString() + "</div>" +
                 "<div>" + totalCart + sumPrice + "</div>" +
+                "<div><a href=\"/webshop\">Back to the shop</a>" +
                 "</body>" +
                 "</html>");
     }
